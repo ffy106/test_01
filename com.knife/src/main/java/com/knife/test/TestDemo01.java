a898ef1233bd2eab45fe9dfa2638b758f08fe22c
ffy <fyfan106@163.com>
2023-05-23 11:46:01 +0800

    架构搭建---01

---
+++ com.knife/src/main/java/com/knife/test/TestDemo01.java
@@ -0,0 +1,17 @@
+package com.knife.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Lx
+ * @version 1.0
+ * @description: TODO
+ * @date 2023/5/23 10:14
+ */
+@SpringBootApplication
+public class TestDemo01 {
+    public static void main(String[] args) {
+        SpringApplication.run(TestDemo01.class,args);
+    }
+}
